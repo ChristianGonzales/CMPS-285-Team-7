5d409dc6c74004b91ba0530351576e4b2289117a
Dustin_Branch.Selu <dustin.branch@selu.edu>
2017-10-17 17:37:24 -0700

    Revert "Got collision to work on right side of screen"
    This reverts commit 6f0558ce98a3bd95acb6158bcad74747a717955d.

--- WebSite1/Game.js
+++ WebSite1/Game.js
@@ -1,4 +1,12 @@
 ﻿//Inspired from: http://www.lostdecadegames.com/how-to-make-a-simple-html5-canvas-game/
+function Character(xPos, yPos, isEnemy) {
+    this.isEnemy = isEnemy;
+    this.width = 100;
+    this.height = 100;
+    this.movementSpeed = 5;
+    this.xPos = xPos;
+    this.yPos = yPos;
+}
 function startGame(characterType) {
 
     var startScreen = document.getElementById("startScreen");
@@ -6,23 +14,20 @@ function startGame(characterType) {
 
     var canvas = document.createElement("canvas");
     var ctx = canvas.getContext("2d");
-    console.log("Here in startGame");
     //Game objects
-    var player = {
-        characterSpriteReady: false,
-        characterSprite: new Image(),
-        xPos: 0,
-        yPos: 0,
-        movementSpeed: 5,
-        draw: function () {
-            ctx.fillRect(this.xPos, this.yPos, 100, 100);
-        }
-    };
+    var player = new Character(0, 530, false);
     var projectile = {
-        projectileImage: new Image(),
-        projectileReady: false,
+        //projectileImage: new Image(),
+        //projectileReady: false,
+        color: "red",
         projectileWidth: 20,
-        projectileHeight: 10
+        projectileHeight: 10,
+        draw: function () {
+            ctx.beginPath();
+            ctx.fillStyle = this.color;
+            ctx.fillRect(300, -150, 20, 10);
+            ctx.closePath();
+        }
     };
     //Key handlersS
     var key = {
@@ -45,50 +50,41 @@ function startGame(characterType) {
     //Other variables
     var lastTime = Date.now();
     var w = window;
+    //Create enemies when E is pressed
+    var createEnemy = function () {
+        var enemy = new Character(300, 0, true);
+        return enemy;
+    }
+    var objective = {
+        currentObjective: "",
+        getObjective: function () {
+            this.currentObjective = "Strap up and get into your first fight!";
+            return this.currentObjective;
+        },
+        drawObjective: function (currentObjective) {
+            ctx.font = "36px Helvetica";
+            ctx.textAlign = "left";
+            ctx.textBaseline = "top";
+            ctx.strokeStyle = "black";
+            ctx.fillStyle = "gold";
+            ctx.fillText(this.currentObjective, 0, 0);
+        }
+    };
     //For multiple browsers Chrome, FireFox, Explorer
     requestAnimationFrame = w.requestAnimationFrame || w.mozRequestAnimationFrame || w.msRequestAnimationFrame;
-    //Event listeners original
-    //w.addEventListener("keydown", function (event) { keysDown[event.keyCode] = true; }, false);
-    //w.addEventListener("keydown", function (event) { delete keysDown[event.keyCode]; }, false);
-    //Event listeners test
+    //Event listeners
     w.addEventListener("keydown", function (event) { key.onKeydown(event); }, false);
     w.addEventListener("keyup", function (event) { key.onKeyup(event); }, false);
-    //Background variables
-    //var backgroundReady = false;
-    //var backgroundImage = new Image();
 
     //Change canvas width and height to whole screen
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     document.body.appendChild(canvas); //Makes it to where the canvas is apart of the HTML body
 
-    //Displaying background
-    //backgroundImage.onload = function () {
-    //    backgroundReady = true;
-    //};
-    //backgroundImage.src = "WebSite1/testBackground.png";
-
-    //Displaying character sprites
-    if (characterType == 1) {
-        player.characterSprite.onload = function () {
-            player.characterSpriteReady = true;
-        }
-        player.characterSprite.src = "../WebSite1/TankSprite/__SCML/3_KNIGHT/3_knight_.png";
-    }
-    //Projectile sprite
-    projectile.projectileImage.onload = function () {
-        projectile.projectileReady = true;
-    }
-    projectile.projectileImage.src = ("../WebSite1/ElfSprite/_SCML/1/arrow.png");
     //When everything gets redrawn on canvas
     var update = function () {
-        //Variables for sides of the canvas
-        var top = canvas.hegiht;
-        var bottom = canvas.hegiht - canvas.height;
-        var rightSide = canvas.width;
-        var leftSide = canvas.width - canvas.width;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        resize();
+        resize();        
         //if (((player.xPos || player.yPos) <= (canvas.width || canvas.height))) {
             if (key.isDown(key.UP)) {
                 player.yPos -= player.movementSpeed
@@ -103,22 +99,9 @@ function startGame(characterType) {
                 player.xPos += player.movementSpeed
             }
             if (key.isDown(key.BATTLE)) {
-                battleLoop();
+                createEnemy();
+                console.log(enemy);
             }
-        //}
-        //else {
-        //    if (player.yPos >= top) {
-        //        player.yPos = top;
-        //    }
-        //    if (player.yPos <= bottom) {
-        //        player.yPos = bottom;
-        //    }
-        //    if (player.xPos >= leftSide) {
-        //        player.yPos = leftSide;
-        //    }
-        //    if (player.yPos >= rightSide) {
-        //        player.yPos = rightSide;
-        //    }
     }
     //Reszie canvas to broswer no matter what
     var resize = function () {
@@ -127,30 +110,23 @@ function startGame(characterType) {
     }
     //Drawing everything
     var render = function () {
-        //if (backgroundReady) {
-        //    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-        //}
-        //if (characterSpriteReady) {
-        //    ctx.drawImage(player.characterSprite, player.xPos, player.yPos);
-        //}
-        if (projectile.projectileReady) {
-            ctx.drawImage(projectile.projectileImage, 500, -200, projectile.projectileWidth, projectile.projectileHeight);
-            console.log("Here");
+        //Draw objective
+        objective.currentObjective = objective.getObjective();
+        objective.drawObjective(objective.currentObjective);
+        if (Character.isEnemy) {
+            ctx.fillStyle = "brown";
+            ctx.fillRect(enemy.xPos, enemy.yPos, enemy.width, enemy.height);
         }
-        player.draw();
-        //enemy.draw();
+        else {
+            //If you need to change the color of the rectangle.
+            ctx.fillStyle = "purple";
+            ctx.fillRect(player.xPos, player.yPos, player.width, player.height);
+        }
+        projectile.draw();
     }
     //When player is in battle
     var battleLoop = function () {
         console.log("In battleLoop");
-        var enemy = {
-            xPos: 200,
-            yPos: -200,
-            draw: function () {
-                ctx.fillStyle = "brown";
-                ctx.fillRect(this.xPos, this.yPos, 100, 100);
-            }
-        };
     }
     //Player walking around map
     var mainGameLoop = function () {
@@ -167,3 +143,67 @@ function startGame(characterType) {
     };
     mainGameLoop();
 }
+
+
+/*
+Test code for different parts of program that might be reused..
+    //Background variables
+    //var backgroundReady = false;
+    //var backgroundImage = new Image();
+
+    //Displaying background
+    //backgroundImage.onload = function () {
+    //    backgroundReady = true;
+    //};
+    //backgroundImage.src = "WebSite1/testBackground.png";
+
+    //Displaying character sprites
+    if (characterType == 1) {
+        player.characterSprite.onload = function () {
+            player.characterSpriteReady = true;
+        }
+        player.characterSprite.src = "../WebSite1/TankSprite/__SCML/3_KNIGHT/3_knight_.png";
+    }
+    //Projectile sprite
+    //projectile.projectileImage.onload = function () {
+    //    projectile.projectileReady = true;
+    //}
+    //projectile.projectileImage.src = ("../WebSite1/ElfSprite/_SCML/1/arrow.png");
+
+
+//Insdie update function for collsion detectioin for when box goes to corner of screen
+    //Variables for sides of the canvas
+        //var top = canvas.hegiht;
+        //var bottom = canvas.hegiht - canvas.height;
+        //var rightSide = canvas.width;
+        //var leftSide = canvas.width - canvas.width;
+
+        //if (((player.xPos || player.yPos) <= (canvas.width || canvas.height))) {
+                //Code for key presses
+        //}
+        //else {
+        //    if (player.yPos >= top) {
+        //        player.yPos = top;
+        //    }
+        //    if (player.yPos <= bottom) {
+        //        player.yPos = bottom;
+        //    }
+        //    if (player.xPos >= leftSide) {
+        //        player.yPos = leftSide;
+        //    }
+        //    if (player.yPos >= rightSide) {
+        //        player.yPos = rightSide;
+        //    }
+
+    Inside the render function
+         //if (backgroundReady) {
+        //    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+        //}
+        //if (characterSpriteReady) {
+        //    ctx.drawImage(player.characterSprite, player.xPos, player.yPos);
+        //}
+        //if (projectile.projectileReady) {
+        //    ctx.drawImage(projectile.projectileImage, 500, -200, projectile.projectileWidth, projectile.projectileHeight);
+        //    console.log("Here");
+        //}
+*/
